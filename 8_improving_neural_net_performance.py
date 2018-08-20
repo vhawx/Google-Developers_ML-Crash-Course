7b4148ee639bacdec0fe8063c7b9e78229621caa
Victoria <19959252+vhawx@users.noreply.github.com>
2018-08-20 17:16:44 -0400

    Add files via upload

--- 8_improving_neural_net_performance.py
+++ 8_improving_neural_net_performance.py
@@ -26,7 +26,7 @@ Step 1: Load data
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
-pd.options.display.float_format = '{:.1f}'.formats
+pd.options.display.float_format = '{:.1f}'.format
 
 california_housing_dataframe = pd.read_csv("https://dl.google.com/mlcc/mledu-datasets/california_housing_train.csv", sep=",")
 
@@ -145,6 +145,122 @@ def train_nn_regression_model(
     training_targets,
     validation_examples,
     validation_targets):
+  """Trains a NN regression model.
+  
+  In addition to training, this function prints training progress information,
+  & a plot of the training and validation loss over time.
+  
+  Args:
+    my_optimizer: An instance of `tf.train.Optimizer`, the optimizer to use.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    hidden_units: A `list` of int values, specifying the number of neurons in each layer.
+    training_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for training.
+    training_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for training.
+    validation_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for validation.
+    validation_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for validation.
+      
+  Returns:
+    A tuple `(estimator, training_losses, validation_losses)`:
+      estimator: the trained `DNNRegressor` object.
+      training_losses: a `list` containing the training loss values taken during training.
+      validation_losses: a `list` containing the validation loss values taken during training.
+  """
+
+  periods = 10
+  steps_per_period = steps / periods
+  
+  # Create a DNNRegressor object
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  dnn_regressor = tf.estimator.DNNRegressor(
+      feature_columns=construct_feature_columns(training_examples),
+      hidden_units=hidden_units,
+      optimizer=my_optimizer
+  )
+  
+  # Create input functions.
+  training_input_fn = lambda: my_input_fn(training_examples, 
+                                          training_targets["median_house_value"], 
+                                          batch_size=batch_size)
+  predict_training_input_fn = lambda: my_input_fn(training_examples, 
+                                                  training_targets["median_house_value"], 
+                                                  num_epochs=1, 
+                                                  shuffle=False)
+  predict_validation_input_fn = lambda: my_input_fn(validation_examples, 
+                                                    validation_targets["median_house_value"], 
+                                                    num_epochs=1, 
+                                                    shuffle=False)
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("RMSE (on training data):")
+  training_rmse = []
+  validation_rmse = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    dnn_regressor.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+    # Take a break and compute predictions.
+    training_predictions = dnn_regressor.predict(input_fn=predict_training_input_fn)
+    training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+    
+    validation_predictions = dnn_regressor.predict(input_fn=predict_validation_input_fn)
+    validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+    
+    # Compute training and validation loss.
+    training_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(training_predictions, training_targets))
+    validation_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(validation_predictions, validation_targets))
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+    # Add the loss metrics from this period to our list.
+    training_rmse.append(training_root_mean_squared_error)
+    validation_rmse.append(validation_root_mean_squared_error)
+  print("Model training finished.")
+
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("RMSE")
+  plt.xlabel("Periods")
+  plt.title("Root Mean Squared Error vs. Periods")
+  plt.tight_layout()
+  plt.plot(training_rmse, label="training")
+  plt.plot(validation_rmse, label="validation")
+  plt.legend()
+  #plt.show()
+
+  print("Final RMSE (on training data):   %0.2f" % training_root_mean_squared_error)
+  print("Final RMSE (on validation data): %0.2f" % validation_root_mean_squared_error)
+
+  return dnn_regressor, training_rmse, validation_rmse
+
+_ = train_nn_regression_model(
+    my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0007),
+    steps=5000,
+    batch_size=70,
+    hidden_units=[10, 10],
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
+
+def train_nn_regression_model(
+    my_optimizer,
+    steps,
+    batch_size,
+    hidden_units,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
   """Trains a neural network regression model.
   
   In addition to training, this function also prints training progress information,
@@ -241,12 +357,176 @@ def train_nn_regression_model(
 
   return dnn_regressor, training_rmse, validation_rmse
 
+'''
+Linear Scaling
+A good standard practice is to normalize the inputs to fall within the range -1, 1.
+This helps SGD not get stuck taking steps that are too large in one dimension, or too small in another.
+Fans of numerical optimization may note that there's a connection to the idea of using a preconditioner here.
+'''
+
+def linear_scale(series):
+  min_val = series.min()
+  max_val = series.max()
+  scale = (max_val - min_val) / 2.0
+  return series.apply(lambda x:((x - min_val) / scale) - 1.0)
+
+'''
+Task 1: Normalize the Features Using Linear Scaling
+Normalize the inputs to the scale -1, 1.
+Spend about 5 minutes training and evaluating on the newly normalized data. How well can you do?
+As a rule of thumb, NN's train best when the input features are roughly on the same scale.
+Sanity check your normalized data. (What would happen if you forgot to normalize one feature?)
+
+Since normalization uses min and max, we have to ensure it's done on the entire dataset at once.
+We can do that here because all our data is in a single DataFrame. If we had multiple data sets,
+a good practice would be to derive the normalization parameters from the training set and apply those identically to the test set.
+'''
+
+def normalize_linear_scale(examples_dataframe):
+  """Returns a version of the input `DataFrame` that has all its features normalized linearly."""
+  processed_features = pd.DataFrame()
+  processed_features["latitude"] = linear_scale(examples_dataframe["latitude"])
+  processed_features["longitude"] = linear_scale(examples_dataframe["longitude"])
+  processed_features["housing_median_age"] = linear_scale(examples_dataframe["housing_median_age"])
+  processed_features["total_rooms"] = linear_scale(examples_dataframe["total_rooms"])
+  processed_features["total_bedrooms"] = linear_scale(examples_dataframe["total_bedrooms"])
+  processed_features["population"] = linear_scale(examples_dataframe["population"])
+  processed_features["households"] = linear_scale(examples_dataframe["households"])
+  processed_features["median_income"] = linear_scale(examples_dataframe["median_income"])
+  processed_features["rooms_per_person"] = linear_scale(examples_dataframe["rooms_per_person"])
+  return processed_features
+
+normalized_dataframe = normalize_linear_scale(preprocess_features(california_housing_dataframe))
+normalized_training_examples = normalized_dataframe.head(12000)
+normalized_validation_examples = normalized_dataframe.tail(5000)
+
 _ = train_nn_regression_model(
     my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0007),
     steps=5000,
     batch_size=70,
     hidden_units=[10, 10],
-    training_examples=training_examples,
+    training_examples=normalized_training_examples,
     training_targets=training_targets,
-    validation_examples=validation_examples,
+    validation_examples=normalized_validation_examples,
+    validation_targets=validation_targets)
+
+'''
+Task 2: Try a Different Optimizer: Adagrad and Adam optimizers.
+
+Adagrad optimizer.
+modifies the learning rate adaptively for each coefficient in a model, monotonically lowering the effective learning rate.
+Works great for convex problems, but isn't always ideal for the non-convex problem NN training.
+use Adagrad by specifying AdagradOptimizer instead of GradientDescentOptimizer.
+Note: may need a larger learning rate with Adagrad.
+
+Adam optimizer.
+For non-convex optimization problems, Adam is sometimes more efficient than Adagrad.
+To use Adam, invoke the tf.train.AdamOptimizer method.
+This method takes several optional hyperparameters as arguments, but our solution only specifies one of these (learning_rate).
+In a production setting, specify and tune the optional hyperparameters carefully.
+'''
+
+'''
+Adagrad optimizer
+'''
+_, adagrad_training_losses, adagrad_validation_losses = train_nn_regression_model(
+    my_optimizer=tf.train.AdagradOptimizer(learning_rate=0.5),
+    steps=500,
+    batch_size=100,
+    hidden_units=[10, 10],
+    training_examples=normalized_training_examples,
+    training_targets=training_targets,
+    validation_examples=normalized_validation_examples,
+    validation_targets=validation_targets)
+
+'''
+Adam optimizer
+'''
+_, adam_training_losses, adam_validation_losses = train_nn_regression_model(
+    my_optimizer=tf.train.AdamOptimizer(learning_rate=0.009),
+    steps=500,
+    batch_size=100,
+    hidden_units=[10, 10],
+    training_examples=normalized_training_examples,
+    training_targets=training_targets,
+    validation_examples=normalized_validation_examples,
+    validation_targets=validation_targets)
+'''
+print a graph of loss metrics side by side.
+'''
+plt.ylabel("RMSE")
+plt.xlabel("Periods")
+plt.title("Root Mean Squared Error vs. Periods")
+plt.plot(adagrad_training_losses, label='Adagrad training')
+plt.plot(adagrad_validation_losses, label='Adagrad validation')
+plt.plot(adam_training_losses, label='Adam training')
+plt.plot(adam_validation_losses, label='Adam validation')
+_ = plt.legend()
+#plt.show()
+
+'''
+Task 3: Explore Alternate Normalization Methods
+Try alternate normalizations for various features to further improve performance.
+Look closely at summary stats for your transformed data, notice that linear scaling some features leaves them clumped close to -1.
+For example, many features have a median of -0.8 or so, rather than 0.0.
+'''
+_ = normalized_training_examples.hist(bins=20, figsize=(18, 12), xlabelsize=10)
+'''
+We might be able to do better by choosing additional ways to transform these features.
+For example, a log scaling might help some features.
+Or clipping extreme values may make the remainder of the scale more informative.
+'''
+def log_normalize(series):
+  return series.apply(lambda x:math.log(x+1.0))
+
+def clip(series, clip_to_min, clip_to_max):
+  return series.apply(lambda x:(
+    min(max(x, clip_to_min), clip_to_max)))
+
+def z_score_normalize(series):
+  mean = series.mean()
+  std_dv = series.std()
+  return series.apply(lambda x:(x - mean) / std_dv)
+
+def binary_threshold(series, threshold):
+  return series.apply(lambda x:(1 if x > threshold else 0))
+'''
+The block above contains a few additional possible normalization functions.
+Try some of these, or add your own.
+Note that if you normalize the target, you'll need to un-normalize the predictions for loss metrics to be comparabl
+These are only a few ways in which we could think about the data. Other transformations may work even better!
+households, median_income and total_bedrooms all appear normally-distributed in a log space.
+latitude, longitude and housing_median_age would probably be better off just scaled linearly, as before.
+population, totalRooms and rooms_per_person have a few extreme outliers. They seem too extreme for log normalization to help. So let's clip them instead.
+'''
+def normalize(examples_dataframe):
+  """Returns a version of the input `DataFrame` that has all its features normalized."""
+  processed_features = pd.DataFrame()
+
+  processed_features["households"] = log_normalize(examples_dataframe["households"])
+  processed_features["median_income"] = log_normalize(examples_dataframe["median_income"])
+  processed_features["total_bedrooms"] = log_normalize(examples_dataframe["total_bedrooms"])
+  
+  processed_features["latitude"] = linear_scale(examples_dataframe["latitude"])
+  processed_features["longitude"] = linear_scale(examples_dataframe["longitude"])
+  processed_features["housing_median_age"] = linear_scale(examples_dataframe["housing_median_age"])
+
+  processed_features["population"] = linear_scale(clip(examples_dataframe["population"], 0, 5000))
+  processed_features["rooms_per_person"] = linear_scale(clip(examples_dataframe["rooms_per_person"], 0, 5))
+  processed_features["total_rooms"] = linear_scale(clip(examples_dataframe["total_rooms"], 0, 10000))
+
+  return processed_features
+
+normalized_dataframe = normalize(preprocess_features(california_housing_dataframe))
+normalized_training_examples = normalized_dataframe.head(12000)
+normalized_validation_examples = normalized_dataframe.tail(5000)
+
+_ = train_nn_regression_model(
+    my_optimizer=tf.train.AdagradOptimizer(learning_rate=0.15),
+    steps=1000,
+    batch_size=50,
+    hidden_units=[10, 10],
+    training_examples=normalized_training_examples,
+    training_targets=training_targets,
+    validation_examples=normalized_validation_examples,
     validation_targets=validation_targets)
